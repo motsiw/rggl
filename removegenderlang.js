5acc1e326b5e906f61f7284f76c47622e068c68e
motsiw <85445812+motsiw@users.noreply.github.com>
2021-06-06 22:22:21 +0200

    Update removegenderlang.js

--- removegenderlang.js
+++ removegenderlang.js
@@ -77,6 +77,7 @@ function removeGender(sollrot) {
 	  str = str.replace(/(b|B)ewunder(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$ewunderer$2");
 	  str = str.replace(/(h|H)erausforder(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$erausforderer$2");
 	  str = str.replace(/(h|H)inder(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$inderer$2");
+	  str = str.replace(/(p|P)lauder(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$lauderer$2");
 	  
 	  // aus ein(e) wird ein, aus Liebe:r wird Lieber, zusammen mit Nomen:
 	  str = str.replace(/(ein|Ein)(\:e|\/\-e|\*e|\_e|\(e\)) (.*?)(\:in|\*in|\_in|\/\-in|\(in\))([ ,).“"!\--]|$)/g,rot1+"$1 $3$5"+rot2);
