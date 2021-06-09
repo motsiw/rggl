0883603b3e620e89b2453c1cdaeca84ca9bf8a36
motsiw <85445812+motsiw@users.noreply.github.com>
2021-06-09 20:32:26 +0200

    Add files via upload

---
+++ src_edge_chrome/README.md
@@ -0,0 +1,102 @@
+
+Remove German Gender Language
+-----------------------------
+
+v2.8 (xx.06.2021)
+Autor: Motsi Weech (motsi.weech ät protonmail.com)
+
+Kleines Add-On für den Microsoft Edge Chromium.
+Lizenz: GNU GENERAL PUBLIC LICENSE Version 3
+
+Das Add-On entfernt folgendes:
+* Gender-Sternchen (Anwender*innen)
+* Gender-Doppelpunkt (Anwender:innen)
+* Binnenmajuskel (AnwenderInnen)
+* Gendergap (Anwender_innen)
+* Schrägstrichlösung (Anwender/-innen)
+* Klammerlösung (Anwender(innen))
+
+Viele Spezialfälle werden berücksichtigt. Dadurch werden deutschsprachige Webseiten besser lesbar dargestellt. 
+Das gilt auch für Zeitungs-Webseiten wie heise, taz, Die Zeit, Frankfurter Rundschau usw.
+
+Bedienung:
+In der Symbolleiste des Firefox erscheint ein neuer "Gender"-Knopf. Er wechselt die Farbe beim Klicken.
+- schwarz: Gender-Konstruktionen werden gefiltert
+- rot: Gender-Konstruktionmen werden gefiltert und die Änderungen farbig markiert
+- x: Deaktiviert
+- Der Knopf zeigt die Zahl der geänderten Elemente auf der aktuellen Webseite.
+- In der Konsole (F12) lassen sich alle Änderungen im Detail nachvollziehen (Tipp: Filtern nach rggl).
+
+Installation:
+https://microsoftedge.microsoft.com/addons/
+
+
+Version History
+
+2.8
+- Ergänzt: Nomaden
+
+2.7
+- Muslime, Kurden, Wölfinnen, Türken, Gefährten, Freund*innenkreis, 
+- Irinnen, Afghaninnen, Ungarinnen, Kasachinnen, Kongolesinnen, Rumänen
+- Sonderbehandlung taz: Vokale + Trema werden zu Umlauten konvertiert
+
+2.6
+- Performance verbessert
+- Bugfix
+
+2.5
+- Sonderfälle ergänzt 
+  (Wilderer, Zauberer, Plünderer, Bewunderer, Herausforderer, Hinderer, Plauderer, Meuterer, Veräußerer)
+- Verbesserung bei weichen Trennstrichen (taz)
+- farbliche Hervorhebung verbessert
+- schönere Icons
+
+2.4
+- erste öffentliche Version
+
+2.3
+- Code aufgeräumt
+
+2.2
+- Kleinere Korrekturen
+
+2.1
+- Bessere Farben für Hervorhebung von Änderungen
+
+2.0
+- Button zeigt jetzt die Zahl der Änderungen mittels Badge
+- Button schaltet zwischen drei Modi:
+  - Filterung aktiv (Standard) => Symbol schwarz
+  - Änderungen farbig hervorheben => Symbol rot
+  - Deaktiviert => Symbol "ausge-X-t"
+
+1.9
+- Zahlwörter hinzughefügt (Mit 30.000 Einwohner*innen... => Mit 30.000 Einwohnern)
+
+1.8
+- Beamt*innen hinzugefügt
+- Sonderfälle mit Umlaute in den weiblichen Formen
+- Nachbarn hinzugefügt
+
+1.7
+- Vorfahr*innen/Nachfahr*innen hinzugefügt
+- man/frau und frau:man
+- Dativ + 'zufolge' hinzugefügt
+
+1.6:
+- weiche/bedingte Trennstriche verbessert
+- Analyst*innen hinzugefügt
+
+1.5:
+- Zeitmessung hinzugefügt
+- Konsolenausgabe farbig und auf das Wesentliche reduziert
+- weiche/bedingte Trennstriche werden unterstützt
+
+1.4:
+- kleinere Fehler beseitigt
+- Bäuer*innen werden unterstützt
+
+1.3:
+- Knopf hinzugefügt (tut aber noch nichts)
+- diverse sprachliche Korrekturen
---
+++ src_edge_chrome/removegenderlang.js
@@ -0,0 +1,286 @@
+// Remove German Gender Language
+// v2.8
+// License: GPL v3.0
+
+
+// Einstellungen laden
+chrome.storage.local.get(function(settings) {
+  if (settings.aktiv) removeGender(settings.aktiv);
+});
+
+
+function removeGender(sollrot) {
+
+	// Zeitnahme der Abarbeitung starten
+	console.time('RGGL');
+
+	var result = document.querySelectorAll("span, p, li, i, b, em, a, h1, h2, h3, h4, h5, h6, figcaption, td, blockquote");
+
+	console.log("RGGL - prüfe "+result.length+" Seitenelemente");
+	var i;
+	var str;
+	var rot1="";
+	var rot2="";
+	var rot3="";
+	var oldstr="";
+	var changes=0;
+	var x=0;
+	var oldohnehrefclassstyle="";
+	var strohnehrefclassstyle="";
+
+	 
+	 if (sollrot == 2) {
+		rot1="<span style='color:blue;background-color: yellow;'>";
+		rot2="</span>";
+		rot3="_";
+	 } 
+	
+	for (i = 0; i < result.length; i++) {
+	  str = result[i].innerHTML;
+	  
+	  // Sonderbehandlung taz: Weiche Trennstriche entfernen
+	  str = str.replace(/­/g,"");
+	  // Sonderbehandlung taz / PS-Fonts: Vokale plus Trema zu richtigen Umlauten machen
+	  str = str.replace(/u\u0308/g,"ü");
+	  str = str.replace(/U\u0308/g,"Ü");
+	  str = str.replace(/o\o0308/g,"ö");
+	  str = str.replace(/O\O0308/g,"Ö");
+	  str = str.replace(/ä\a0308/g,"ä");
+	  str = str.replace(/Ä\A0308/g,"Ä");
+	  oldstr = str;
+		
+	  
+	  // Konstruktionen wie Der/die oder sie:er durch das generische Maskulinum ersetzen
+	  str = str.replace(/(der|Der)(\/|\_|\:|\*)die([ ,).“"!-])/g,rot1+"$1$3"+rot2);
+	  str = str.replace(/er(\/|\_|\:|\*)sie([ ,).“"!-]|$)/g,rot1+"man$2"+rot2);
+	  str = str.replace(/Er(\/|\_|\:|\*)sie([ ,).“"!-]|$)/g,rot1+"Man$2"+rot2);
+	  str = str.replace(/(die|Die)(\/|\_|\:|\*)der([ ,).“"!-])/g,rot1+"der$3"+rot2);
+	  str = str.replace(/(sie|Sie)(\/|\_|\:|\*)er([ ,).“"!-]|$)/g,rot1+"man$3"+rot2);
+	  str = str.replace(/(dem|Dem)(\/|\_|\:|\*)der([ ,).“"!-]|$)/g,rot1+"dem$3"+rot2);
+	  str = str.replace(/(der|Der)(\/|\_|\:|\*)dem([ ,).“"!-])/g,rot1+"dem$3"+rot2); 
+	  str = str.replace(/(ein|Ein)(\/|\_|\:|\*)eine([ ,).“"!-]|$)/g,rot1+"ein$3"+rot2);
+	  str = str.replace(/(eine|Eine)(\/|\_|\:|\*)ein([ ,).“"!-])/g,rot1+"ein$3"+rot2); 
+
+
+
+	  // Spezialfälle mit Umlauten je nach Geschlecht lösen
+	  str = str.replace(/(b|B)äuer(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$1auer$2");
+	  str = str.replace(/(n|N)ärr(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$1arr$2");
+	  str = str.replace(/(h|H)ünd(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$1und$2");
+	  str = str.replace(/(k|K)ätz(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$1atz$2");
+	  str = str.replace(/(f|F)ranzös(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$1ranzos$2");
+	  str = str.replace(/(p|P)rinzess(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$1rinz$2");
+	  str = str.replace(/(i|I)srael(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$1sraelis$2");
+	  str = str.replace(/(g|G)räf(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$1raf$2");
+	  str = str.replace(/(s|S)chwäger(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$1chwager$2");
+	  str = str.replace(/(w|W)estfäl(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$1estfal$2");
+	  str = str.replace(/(s|S)chwän(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$1chwan$2");
+	  
+	  
+	  
+	  // Spezialfälle: Wörter, die auf -erer/-erin enden
+	  str = str.replace(/(w|W)ander(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$1anderer$2");
+	  str = str.replace(/(f|F)örder(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$1örderer$2");
+	  str = str.replace(/(e|E)rober(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$1roberer$2");
+	  str = str.replace(/(l|L)äster(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$1ästerer$2");
+	  str = str.replace(/(r|R)uder(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$uderer$2");
+	  str = str.replace(/(s|S)chlender(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$chlenderer$2");
+	  str = str.replace(/(w|W)ilder(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$ilderer$2");
+	  str = str.replace(/(k|K)letter(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$letterer$2");
+	  str = str.replace(/(z|Z)auber(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$auberer$2");
+	  str = str.replace(/(p|P)lünder(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$lünderer$2");
+	  str = str.replace(/(b|B)ewunder(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$ewunderer$2");
+	  str = str.replace(/(h|H)erausforder(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$erausforderer$2");
+	  str = str.replace(/(h|H)inder(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$inderer$2");
+	  str = str.replace(/(p|P)lauder(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$lauderer$2");
+	  str = str.replace(/(m|M)euter(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$euterer$2");
+	  str = str.replace(/(v|V)eräußer(\:in|\*in|\/\-in|\_in|\(in|In)/g,"$eräußerer$2");
+	  
+	  
+	  
+	  // aus ein(e) wird ein, aus Liebe:r wird Lieber, zusammen mit Nomen:
+	  str = str.replace(/(ein|Ein)(\:e|\/\-e|\*e|\_e|\(e\)) (.*?)(\:in|\*in|\_in|\/\-in|\(in\))([ ,).“"!\--]|$)/g,rot1+"$1 $3$5"+rot2);
+	  str = str.replace(/(eine|Eine)(\:n|\/\-n|\*n|\_n|\(n\))([ ,).“"!\--]|$)/g,rot1+"$1n$3"+rot2);
+	  str = str.replace(/(jede|liebe|Jede|Liebe|geehrte|Geehrte)(\:r|\/\-r|\*r|\_r|\(r\)) (.*?)(\:in|\*in|\_in|\/\-in|\(in\))([ ,).“"!\--]|$)/g,rot1+"$1r $3$5"+rot2);
+	  str = str.replace(/(ein|Ein)(\:e|\/\-e|\*e|\_e|\(e\))([ ,).“"!\--]|$)/g,rot1+"$1$3"+rot2);
+	  
+	  
+	  
+	  // freistehende jede(r), jede_r, manche(r)
+	  str = str.replace(/(jede|Jede|manche|Manche|keine|Keine|eine|Eine|einzelne|Einzelne|einzige|Einzige|eigene|Eigene)(\:r|\/\-r|\*r|\_r|\(r\))([ ,).“"!\--]|$)/g,rot1+"$1r$3"+rot2);
+	  
+	  
+	  
+	  // frau
+	  str = str.replace(/ frau([ ,).“"!\--]|$)/g,rot1+" man$1"+rot2);
+	  str = str.replace(/(man|Man)(\/|\_|\:|\*)frau([ ,).“"!-])/g,rot1+"$1$3"+rot2);
+	  str = str.replace(/(Frau)(\/|\_|\:|\*)man([ ,).“"!-])/g,rot1+"Man$3"+rot2);
+	  str = str.replace(/(frau)(\/|\_|\:|\*)man([ ,).“"!-])/g,rot1+"man$3"+rot2);
+	  
+	  
+	  
+	  // Jüdinnen/Juden bzw. Juden/Jüdinnen
+	  str = str.replace(/([ ,).“"!-])([a-zA-ZüöäßÜÖÄ]*?)(innen)[\/\*\:\-\_]([a-zA-ZüöäßÜÖÄ]*?)(en)([ ,).“"!-])/g,rot1+"$1$4$5$6"+rot2);
+	  str = str.replace(/([ ,).“"!-])([a-zA-ZüöäßÜÖÄ]*?)(en)[\/\*\:\-\_]([a-zA-ZüöäßÜÖÄ]*?)(innen)([ ,).“"!-])/g,rot1+"$1$4$5$6"+rot2);
+	  
+	  
+	  
+	  // geklammertes Binnen-(inn) entfernen
+	  str = str.replace(/([a-züöäß])\(inn\)([a-züöäß]|$)/g,rot1+"$1$2"+rot2);
+	  
+	  
+	  
+	  // Sonderkonstrukt Staatsbürger:innenschaft
+	  str = str.replace(/([a-züöäß])(\*|\:|\_)innen(schaft)([ ,).“"!\--]|$)/g,rot1+"$1$3$4"+rot2);
+	  str = str.replace(/([a-züöäß])Innen(schaft)([ ,).“"!\--]|$)/g,rot1+"$1$2$3"+rot2);
+	  // Sonderkonstrukt Freun*Innenkreis
+	  str = str.replace(/(freund|Freund)(\*|\:|\_)innen(kreis)([ ,).“"!\--]|$)/g,rot1+"$1es$3$4"+rot2);
+	  str = str.replace(/(freund|Freund)Innen(kreis)([ ,).“"!\--]|$)/g,rot1+"$1es$2$3"+rot2);
+	  
+	  
+	  
+
+	  // und hier die Mehrzahl für Bauern und Nachbarn
+	  str = str.replace(/(bauer|achbar)(\:|\*|\/\-|\_)(innen)([ ,).“"!\--]|$)/gi,rot1+"$1n$4"+rot2);
+	  str = str.replace(/([A-Za-zÜÖÄüöäß])(Bauer|bauer|achbar)(Innen)([ ,).“"!\--]|$)/g,rot1+"$1$2n$4"+rot2);
+	  
+	  // Muslime
+	  str = str.replace(/(uslim)(\:|\*|\/\-|\_)(innen)([ ,).“"!\--]|$)/gi,rot1+"$1e$4"+rot2);
+	  str = str.replace(/([A-Za-zÜÖÄüöäß])(uslim)(Innen)([ ,).“"!\--]|$)/g,rot1+"$1$2e$4"+rot2);
+	  
+
+	  // Bei Nomen, die auf -eur, -pst oder -bst enden, wird das :innen durch en ersetzt
+	  // wenn ein Vorsatzwort den Dativ vorgibt
+	  // Beispiel: Ingenieur:innen => Ingenieure, DomteurInnen => Dompteure, Päpst:innen => Päpste
+	  str = str.replace(/(den|von|mit|auf|unter|einen|chen|ten|len|che|ren|vor|zu|igen|aus)(| [0-9,.]*?| zwei| drei| vier| fünf| sechs| sieben| acht| neun| zehn| elf| zwölf| zwanzig| dreißig| vierzig| fünfzig| hundert| tausend) ([A-Za-züöäÖÜÄß]*?)(eur|pst|bst|hund|dar|wölf)(\:|\*|\/\-|\_)(innen)([ ,).“"!\--]|$)/gi,rot1+"$1$2 $3$4en$7"+rot2);
+	  str = str.replace(/(den|von|mit|auf|unter|einen|chen|ten|len|che|ren|vor|zu|igen|aus)(| [0-9,.]*?| zwei| drei| vier| fünf| sechs| sieben| acht| neun| zehn| elf| zwölf| zwanzig| dreißig| vierzig| fünfzig| hundert| tausend) ([A-Za-züöäÖÜÄß]*?)(eur|pst|bst|hund|Hund|dar|Wölf|wölf)(Innen)([ ,).“"!\--]|$)/g,rot1+"$1$2 $3$4en$6"+rot2);
+
+
+
+	  // Bei Nomen, die auf -eur, -pst oder -bst enden, wird das :innen durch e ersetzt
+	  // Beispiel: Ingenieur:innen => Ingenieure, DomteurInnen => Dompteure, Päpst:innen => Päpste
+	  str = str.replace(/(eur|pst|bst|hund|Hund|dar|wölf|Wölf)(\:|\*|\/\-|\_)(innen)([ ,).“"!\--]|$)/g,rot1+"$1e$4"+rot2);
+	  str = str.replace(/([a-züöäß])(eur|pst|bst|hund|Hund|dar|wölf|Wölf)(Innen)([ ,).“"!\--]|$)/g,rot1+"$1$2e$4"+rot2);
+	  
+	  
+	  
+	  // Bestimmte Vorsatzwörter und -silben deuten auf den Dativ, dann wird *innen zu en, außer das Nomen endet auf 'r'
+	  // Beispiel: unter Therapeut*innen => unter Therapeuten
+	  // gilt auch, wenn eine Zahl oder ein Zahlwort dazwischen steht
+	  
+	  str = str.replace(/(den|von|mit|auf|unter|einen|chen|ten|len|che|ren|vor|zu|igen|aus)(| [0-9,.]*?| zwei| drei| vier| fünf| sechs| sieben| acht| neun| zehn| elf| zwölf| zwanzig| dreißig| vierzig| fünfzig| hundert| tausend) ([a-zA-ZüöäßÜÖÄ]*?[bcdfghjklmnpqstvwxz])(\:innen|\*innen|\_innen|\/\-innen|\(innen\))([ ,).“"!\--]|$)/gi,rot1+"$1$2 $3en$5"+rot2);
+	  str = str.replace(/(den|von|mit|auf|unter|einen|chen|ten|len|che|ren|vor|zu|igen|aus)(| [0-9,.]*?| zwei| drei| vier| fünf| sechs| sieben| acht| neun| zehn| elf| zwölf| zwanzig| dreißig| vierzig| fünfzig| hundert| tausend) ([a-zA-ZüöäßÜÖÄ]*?[bcdfghjklmnpqstvwxz])Innen([ ,).“"!\--]|$)/g,rot1+"$1$2 $3en$4"+rot2);
+	  str = str.replace(/(den|von|mit|auf|unter|einen|chen|ten|len|che|ren|vor|zu|igen|aus)(| [0-9,.]*?| zwei| drei| vier| fünf| sechs| sieben| acht| neun| zehn| elf| zwölf| zwanzig| dreißig| vierzig| fünfzig| hundert| tausend) ([a-zA-ZüöäßÜÖÄ]*?[bcdfghjklmnpqstvwxz])(\:in|\*in|\_in|\/\-in|\(in\))([ ,).“"!\--]|$)/gi,rot1+"$1$2 $3en$5"+rot2);
+	  str = str.replace(/(den|von|mit|auf|unter|einen|chen|ten|len|che|ren|vor|zu|igen|aus)(| [0-9,.]*?| zwei| drei| vier| fünf| sechs| sieben| acht| neun| zehn| elf| zwölf| zwanzig| dreißig| vierzig| fünfzig| hundert| tausend) ([a-zA-ZüöäßÜÖÄ]*?[bcdfghjklmnpqstvwxz])In([ ,).“"!\--]|$)/g,rot1+"$1$2 $3en$4"+rot2);
+	  
+	  
+	  // Ärzte, Freunde und Hunde
+	  str = str.replace(/(hund|ärzt|freund)(\:innen|\*innen|\_innen|\/\-innen|\(innen\))([ ,).“"!\--]|$)/gi,rot1+"$1e$3"+rot2);
+	  str = str.replace(/(Hund|hund|ärzt|Ärzt|Freund|freund)(Innen)([ ,).“"!-]|$)/g,rot1+"$1e$3"+rot2);
+	  
+	  
+	  // Dasselbe für den Genitiv
+	  str = str.replace(/(der|ner|ter|nger)(| [0-9,.]*?| zwei| drei| vier| fünf| sechs| sieben| acht| neun| zehn| elf| zwölf| zwanzig| dreißig| vierzig| fünfzig| hundert| tausend) ([a-zA-ZüöäßÜÖÄ]*?[bcdfghjklmnpqstvwxz])(\:innen|\*innen|\_innen|\/\-innen|\(innen\))([ ,).“"!\--]|$)/gi,rot1+"$1$2 $3en$5"+rot2);
+	  str = str.replace(/(der|ner|ter|nger)(| [0-9,.]*?| zwei| drei| vier| fünf| sechs| sieben| acht| neun| zehn| elf| zwölf| zwanzig| dreißig| vierzig| fünfzig| hundert| tausend) ([a-zA-ZüöäßÜÖÄ]*?[bcdfghjklmnpqstvwxz])Innen([ ,).“"!\--]|$)/g,rot1+"$1$2 $3en$4"+rot2);
+	  
+	  // Beamte
+	  str = str.replace(/(beamt)(\:innen|\*innen|\_innen|\/\-innen|\(innen\))([ ,).“"!\--]|$)/gi,rot1+"$1e$3"+rot2);
+	  str = str.replace(/(Beamt|beamt)(Innen)([ ,).“"!-]|$)/g,rot1+"$1e$3"+rot2);
+	  
+	  
+	  // Bei Nomen, die auf -or, -ist, -leg, -nom usw. enden, wird das :innen durch en ersetzt.
+	  // Beispiel: Astronom*innen => Astronomen, KollegInnen => Kollegen
+	  str = str.replace(/(or|ist|leg|nom|log|pert|naut|fahr|rat|dat|gog|oss|äst|ent|lyst|graf|zos|atz|ess|fal|ant|peut|urd|kund|klient|fährt|ürk|ghan|gar|inn|dän|män|asach|les|sbek|bell|mad)(\:innen|\*innen|\_innen|\/\-innen|\(innen\))([ ,).“"!\--]|$)/gi,rot1+"$1en$3"+rot2);
+	  str = str.replace(/([a-zA-ZüöäÜÖÄß])(or|ist|leg|nom|log|pert|naut|fahr|rat|dat|gog|oss|äst|ent|lyst|graf|zos|atz|ess|fal|ant|peut|urd|und|lient|fährt|ürk|ghan|gar|inn|dän|Dän|män|asach|les|sbek|bell|mad)(Innen)([ ,).“"!\--]|$)/g,rot1+"$1$2en$4"+rot2);
+	  
+	  // Spezialfall Ir*innen
+	  str = str.replace(/ Ir(\:|\*|\/\-|\_|)(innen|Innen)([ ,).“"!\--]|$)/g,rot1+" Iren$3"+rot2);
+	  
+	  
+	  // und hier die Einzahl für Kolleg*in, Astrolog*in, Expert*in, Dämagog*in, Genoss*in, Kätz*in
+	  str = str.replace(/(leg|log|pert|gog|oss|äst|atz)(\:|\*|\/\-|\_)(in)([ ,).“"!\--]|$)/gi,rot1+"$1e$4"+rot2);
+	  str = str.replace(/([a-züöäß])(leg|log|pert|gog|oss|äst|atz)(In)([ ,).“"!\--]|$)/g,rot1+"$1$2e$4"+rot2);
+	  
+	  
+	  
+	  // Konstruktionen mit "zufolge"
+	  str = str.replace(/([a-zA-ZüöäßÜÖÄ]*?)(\:innen|\*innen|\_innen|\/\-innen|\(innen\)) zufolge([ ,).“"!\--]|$)/gi,rot1+"$1n zufolge$3"+rot2);
+	  str = str.replace(/([a-zA-ZüöäßÜÖÄ]*?)(Innen) zufolge([ ,).“"!\--]|$)/g,rot1+"$1n zufolge$3"+rot2);
+	  
+	  
+	  
+	  // Bestimmte Vorsatzwörter und -silben deuten auf den Dativ, dann wird *innen zu n, wenn das Nomen auf 'r' endet
+	  // Beispiel: den Arbeiter:innen => den Arbeitern
+	  // gilt auch, wenn eine Zahl oder ein Zahlwort dazwischen steht
+	  str = str.replace(/(den|von|mit|auf|unter|einen|chen|ten|len|ren|vor|zu|igen|aus)(| [0-9,.]*?| zwei| drei| vier| fünf| sechs| sieben| acht| neun| zehn| elf| zwölf| zwanzig| dreißig| vierzig| fünfzig| hundert| tausend) ([a-zA-ZüöäßÜÖÄ]*?r)(\:innen|\*innen|\_innen|\/\-innen|\(innen\))([ ,).“"!\--]|$)/gi,rot1+"$1$2 $3n$5"+rot2);
+	  str = str.replace(/(den|von|mit|auf|unter|einen|chen|ten|len|ren|vor|zu|igen|aus)(| [0-9,.]*?| zwei| drei| vier| fünf| sechs| sieben| acht| neun| zehn| elf| zwölf| zwanzig| dreißig| vierzig| fünfzig| hundert| tausend) ([a-zA-ZüöäßÜÖÄ]*?r)(Innen)([ ,).“"!\--]|$)/g,rot1+"$1$2 $3n$5"+rot2);
+
+
+
+
+	  // In alle übrigen Fällen wird das *innen schlicht entfernt
+	  // Beispiel: Schüler_innen => Schüler
+	  str = str.replace(/([a-züöäß])(\:innen|\*innen|\_innen|\/\-innen|\(innen\))/g,rot1+"$1"+rot2);
+	  str = str.replace(/([a-züöäß])(Innen)/g,rot1+"$1"+rot2);
+	  
+	  
+	  
+	  // Ein freischwebendes *in wird ebenfalls entfernt
+	  // Beispiel: Lehrer:in => Lehrer
+	  str = str.replace(/([a-züöäß])(\:in|\*in|\_in|\/\-in|\(in\))([ ,).“"!\--]|$)/g,rot1+"$1$3"+rot2);
+	  str = str.replace(/([a-züöäß])In([ ,).“"!\--]|$)/g,rot1+"$1$2"+rot2);
+	  
+	
+	  // Korrektur Isaelisen, Franzos
+	  str = str.replace(/sraelisen/g,"sraelis");
+	  str = str.replace(/ranzos/g,"ranzose");
+	  str = str.replace(/ein Katz([ ,\).“_"!\--]|$)/g,"eine Katze$1");
+
+
+
+	  // prüfen, ob Änderungen innerhalb von href, class oder style-Attributen stattfanden. Wenn ja, wird auf die Korrektur vorsorglich verzichtet
+	  oldohnehrefclassstyle = oldstr.replace(/(href|style|class|src|itemprop|onclick|onmouseover|xmlns|alt)=("|').*?("|')/g,"$1=$2$3");
+	  strohnehrefclassstyle = str.replace(/(href|style|class|src|itemprop|onclick|onmouseover|xmlns|alt)=("|').*?("|')/g,"$1=$2$3");
+
+	  // liefert den ersten Unterschied zwischen zwei Strings
+	  function ersterUnterschied(stringa, stringb)
+	  {
+		var i = 0, fundstelle = 0;
+
+		while (i < stringb.length)
+		{
+		  if (stringa[i] != stringb[i] || i == stringa.length) {
+			 fundstelle = i;
+			 break; 
+		  } else i++;
+		}
+		return fundstelle;
+	  }
+	  
+	  
+	  if (oldohnehrefclassstyle != strohnehrefclassstyle) 
+		{
+			changes++;
+			x=ersterUnterschied(oldohnehrefclassstyle, strohnehrefclassstyle);
+			if (x < 50) x = 50;
+			x-=50;
+			if (oldohnehrefclassstyle.length < 100) console.log("%cRGGL #"+changes+" Tag "+result[i].nodeName+" - old: ..."+oldohnehrefclassstyle.substr(x)+"...", 'color: red;');
+			else console.log("%cRGGL Change #"+changes+" Tag "+result[i].nodeName+" - old: ..."+oldohnehrefclassstyle.substr(x,100)+"...", 'color: red;');
+			if (strohnehrefclassstyle.length < 100) console.log("%cRGGL Change #"+changes+" Tag "+result[i].nodeName+" - new: ..."+strohnehrefclassstyle.substr(x)+"...", 'color: green;');
+			else console.log("%cRGGL Change #"+changes+" Tag "+result[i].nodeName+" - new: ..."+strohnehrefclassstyle.substr(x,100)+"...", 'color: green;');
+			
+			result[i].innerHTML = str;
+		}
+	  }
+	  
+	  // background-Skript über die Zahl der Ersetzungen informieren
+	    chrome.runtime.sendMessage({
+            count: changes,
+            type: "count"
+        });
+	  
+	  // Zeit der Abarbeitung stoppen und Ergebnis in die Konsole schreiben
+	  console.timeEnd('RGGL');
+}
+
+
