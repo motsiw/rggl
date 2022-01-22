1736b1cbda0fb57605b33990d032c3ffc223f4f9
motsiw <85445812+motsiw@users.noreply.github.com>
2022-01-22 20:33:36 +0100

    v5.5

--- removegenderlang.js
+++ removegenderlang.js
@@ -5,19 +5,8 @@
 
 // Einstellungen laden
 browser.storage.local.get(function(settings) {
-  
-  	var docxmllang = document.getElementsByTagName('html')[0].getAttribute('xml:lang');
-	var doclang = document.getElementsByTagName('html')[0].getAttribute('lang');
-	doclang = String(doclang).toLowerCase()+String(docxmllang).toLowerCase();
-	var metalang = document.querySelectorAll('meta[content="de"]');
-	var metalang1 = document.querySelectorAll('meta[content="DE"]');
-	var metalang2 = document.querySelectorAll('meta[content="de-de"]');
-	var metalang3 = document.querySelectorAll('meta[content="de-at"]');
-	var metalang4 = document.querySelectorAll('meta[content="de-DE"]');
-	var metalang5 = document.querySelectorAll('meta[content="de-AT"]');
-	var metalang6 = document.querySelectorAll('meta[content="de-CH"]');
-	var metalang7 = document.querySelectorAll('meta[content="de-ch"]');
-	if (doclang.indexOf("de") != -1 || doclang.indexOf("at") != -1  || doclang.indexOf("ch") != -1 || metalang.length > 0 || metalang1.length > 0 || metalang2.length > 0 || metalang3.length > 0 || metalang4.length > 0 || metalang5.length > 0 || metalang6.length > 0 || metalang7.length > 0)
+
+	if (spracheWebsite())
 	{
 		console.log("RGGL - Deutschsprachige Webseite erkannt.");
 		if (settings.aktiv) removeGender(settings.aktiv);
@@ -32,6 +21,26 @@ browser.storage.local.get(function(settings) {
 	}
 });
 
+// ermitteln, ob Webseite auf Deutsch ist
+function spracheWebsite() {
+  	var rueckgabe=0;
+	var docxmllang = document.getElementsByTagName('html')[0].getAttribute('xml:lang');
+	var doclang = document.getElementsByTagName('html')[0].getAttribute('lang');
+	doclang = String(doclang).toLowerCase()+String(docxmllang).toLowerCase();
+	var metalang = document.querySelectorAll('meta[content="de"]');
+	var metalang1 = document.querySelectorAll('meta[content="DE"]');
+	var metalang2 = document.querySelectorAll('meta[content="de-de"]');
+	var metalang3 = document.querySelectorAll('meta[content="de-at"]');
+	var metalang4 = document.querySelectorAll('meta[content="de-DE"]');
+	var metalang5 = document.querySelectorAll('meta[content="de-AT"]');
+	var metalang6 = document.querySelectorAll('meta[content="de-CH"]');
+	var metalang7 = document.querySelectorAll('meta[content="de-ch"]');
+	if (doclang.indexOf("de") != -1 || doclang.indexOf("at") != -1  || doclang.indexOf("ch") != -1 || metalang.length > 0 || metalang1.length > 0 || metalang2.length > 0 || metalang3.length > 0 || metalang4.length > 0 || metalang5.length > 0 || metalang6.length > 0 || metalang7.length > 0)
+	{
+		rueckgabe = 1;
+	}
+	return rueckgabe;
+}
 
 function removeGender(sollrot) {
 
